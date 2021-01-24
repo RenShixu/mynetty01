3123194d73c9c7c8faf1eaaf1d11e6204426bad3
ren_shixu <ren_shixu@aliyun.com>
2021-01-24 10:08:41 +0800

    c1 修改

--- src/main/java/com/rsx/TestUtil.java
+++ src/main/java/com/rsx/TestUtil.java
@@ -3,5 +3,6 @@ package com.rsx;
 public class TestUtil {
     public static void main(String[] args) {
         System.out.println("main method ...");
+        System.out.println("print 01");
     }
 }
